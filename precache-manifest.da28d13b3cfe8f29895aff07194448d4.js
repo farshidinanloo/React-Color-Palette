8b0b2a6496753a61de7ed5443f9a665e8f74702e
farshid inanloo <farshidinanloo73@gmail.com>
2019-08-29 17:05:07 +0430

    Updates

---
+++ precache-manifest.da28d13b3cfe8f29895aff07194448d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a065007e2ed4e97943bb4cf2780396",
+    "url": "/React-Color-Palette/index.html"
+  },
+  {
+    "revision": "c875e882b163544ae05f",
+    "url": "/React-Color-Palette/static/css/2.e337d257.chunk.css"
+  },
+  {
+    "revision": "60b316b36958ceae41ea",
+    "url": "/React-Color-Palette/static/css/main.98a2b250.chunk.css"
+  },
+  {
+    "revision": "c875e882b163544ae05f",
+    "url": "/React-Color-Palette/static/js/2.b9ec0f81.chunk.js"
+  },
+  {
+    "revision": "60b316b36958ceae41ea",
+    "url": "/React-Color-Palette/static/js/main.64d123f5.chunk.js"
+  },
+  {
+    "revision": "d6796cce3850e41c9fa5",
+    "url": "/React-Color-Palette/static/js/runtime~main.4dc93e59.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/React-Color-Palette/static/media/bg.77428cd4.svg"
+  }
+]);
